6be1bbd798997ac2bb4d33b96264f875090ea947
charlie <the_charlie_daly@hotmail.co.uk>
2016-10-02 16:22:54 +0100

    Started creating login form

--- src/app/components/auth/auth-login/auth-login.ts
+++ src/app/components/auth/auth-login/auth-login.ts
@@ -1,20 +1,26 @@
 import {Component, Output, Input, EventEmitter} from '@angular/core';
 import {IAuthService} from '../../../services/auth';
+import {FormField} from '../../../utils/';
 
 @Component({
   selector: 'auth-login',
   templateUrl: './auth-login.html'
 })
 export class AuthLogin {
-  @Output() onSubmit = new EventEmitter<IAuthLoginFormState>();
+  @Output() onSubmit = new EventEmitter<void>();
+  @Output() onStateChange = new EventEmitter<LoginFormModel>();
   @Input() errors: string[];
   
-  constructor() {}
+  model = new LoginFormModel();
+
+  submit() {
+    this.onStateChange.emit(this.model);
+  }
 
 }
 
-export interface IAuthLoginFormState {
-  email: string;
-  password: string;
-  keepLoggedIn: boolean; 
+export class LoginFormModel {
+    email = new FormField("");
+    password = new FormField("");
+    keepSignedIn = new FormField(false);
 }
--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './random.utils';
+export * from './forms.utils';
